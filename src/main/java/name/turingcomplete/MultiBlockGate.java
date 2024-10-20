524cd427aa47f31b0d83b68d779cc752fdd83781
DutchTristan <tristan@running-gag.nl>
2024-10-20 23:04:22 +0200

    broken half adder :,)

---
+++ src/main/java/name/turingcomplete/MultiBlockGate.java
@@ -0,0 +1,163 @@
+package name.turingcomplete;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
+import net.minecraft.world.tick.TickPriority;
+
+public abstract class MultiBlockGate extends AbstractEnableGate{
+    public static final EnumProperty<BLOCK_PART> PART = EnumProperty.of("part", BLOCK_PART.class);
+
+
+    public static final BooleanProperty SUM = Properties.POWERED;
+    public static final BooleanProperty CARRY = Properties.ENABLED;
+    public static final BooleanProperty HALFSUM = BooleanProperty.of("halfsum");
+    public static final Direction DirToTop = null;
+
+    protected static final VoxelShape SHAPE = VoxelShapes.cuboid(0, 0, 0, 1, 0.125, 1);
+
+
+    public MultiBlockGate(Settings settings) {
+        super(settings);
+        setDefaultState(getDefaultState()
+                .with(CARRY, false)
+                .with(SUM, false)
+                .with(PART,BLOCK_PART.MIDDLE)
+                .with(FACING, Direction.NORTH)
+                .with(HALFSUM, false));
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
+        super.onPlaced(world, pos, state, placer, itemStack);
+        Direction direction = state.get(FACING);
+        if (state.get(PART) == BLOCK_PART.MIDDLE){
+            switch (direction) {
+                case NORTH: {
+                    world.setBlockState(pos.west(), state.with(PART, BLOCK_PART.BOTTOM).with(FACING, Direction.NORTH), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.east(), state.with(PART, BLOCK_PART.TOP).with(FACING, Direction.NORTH), Block.NOTIFY_ALL);
+                    return;
+                }
+                case SOUTH: {
+                    world.setBlockState(pos.west(), state.with(PART, BLOCK_PART.TOP).with(FACING, Direction.SOUTH), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.east(), state.with(PART, BLOCK_PART.BOTTOM).with(FACING, Direction.SOUTH), Block.NOTIFY_ALL);
+                    return;
+                }
+                case EAST: {
+                    world.setBlockState(pos.south(), state.with(PART, BLOCK_PART.TOP).with(FACING, Direction.EAST), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.north(), state.with(PART, BLOCK_PART.BOTTOM).with(FACING, Direction.EAST), Block.NOTIFY_ALL);
+                    return;
+                }
+                case WEST: {
+                    world.setBlockState(pos.south(), state.with(PART, BLOCK_PART.BOTTOM).with(FACING, Direction.WEST), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.north(), state.with(PART, BLOCK_PART.TOP).with(FACING, Direction.WEST), Block.NOTIFY_ALL);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, net.minecraft.block.BlockState> builder) {
+        builder.add(Properties.HORIZONTAL_FACING, SUM, CARRY, HALFSUM, PART);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx)
+    {
+        BlockState state = getDefaultState();
+        state = state.with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+        return state;
+    }
+
+    @Override
+    public BlockState onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player){
+        Direction direction = state.get(FACING).rotateYCounterclockwise();
+        if (state.get(PART) != BLOCK_PART.TOP && state.get(PART) != BLOCK_PART.BOTTOM){
+            BlockPos top = findEnd(world,pos,state);
+            BlockState Top = world.getBlockState(top);
+            BlockPos next;
+            while (Top.get(PART) != BLOCK_PART.BOTTOM && Top.isOf(this)){
+                next = top.offset(direction);
+                world.setBlockState(top, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL);//| Block.SKIP_DROPS);
+                world.syncWorldEvent(player, WorldEvents.BLOCK_BROKEN, top, Block.getRawIdFromState(Top));
+                top = next;
+                Top = world.getBlockState(top);
+            }
+            if (Top.isOf(this)) {
+                world.setBlockState(top, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL);
+                world.syncWorldEvent(player, WorldEvents.BLOCK_BROKEN, top, Block.getRawIdFromState(Top));
+            }
+        } else {
+            BLOCK_PART Start = state.get(PART);
+            BLOCK_PART End = BLOCK_PART.BOTTOM;
+            if (Start == BLOCK_PART.BOTTOM){
+                direction = direction.getOpposite();
+                End = BLOCK_PART.TOP;
+            }
+            BlockPos current = pos;
+            BlockState Current = world.getBlockState(pos);
+            BlockPos next;
+            while (Current.isOf(this) && Current.get(PART) != End){
+                next = current.offset(direction);
+                world.setBlockState(current, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL);// | Block.SKIP_DROPS);
+                world.syncWorldEvent(player, WorldEvents.BLOCK_BROKEN, current, Block.getRawIdFromState(Current));
+                current = next;
+                Current = world.getBlockState(current);
+            }
+            if (Current.isOf(this)) {
+                world.setBlockState(current, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL);
+                world.syncWorldEvent(player, WorldEvents.BLOCK_BROKEN, current, Block.getRawIdFromState(Current));
+            }
+        }
+        return super.onBreak(world,pos,state,player);
+    }
+
+    public BlockPos findEnd(World world, BlockPos pos, BlockState state){
+        Direction direction = state.get(FACING).rotateYClockwise();
+        BlockPos top = pos.offset(direction);
+        BlockState Top = world.getBlockState(top);
+        while (Top.get(PART) != BLOCK_PART.TOP && Top.isOf(this)){
+            top = pos.offset(direction);
+            Top = world.getBlockState(top);
+        }
+        return top;
+    }
+
+    public int getSideInput(World world, BlockState state, BlockPos pos){
+        Direction redstoneDir = state.get(FACING);
+        BlockPos redstonePos = pos.offset(redstoneDir);
+        int output = getInput(world,redstonePos,redstoneDir);
+        return output;
+    }
+
+    @Override
+    protected boolean hasPower(World world, BlockPos pos, BlockState state) {
+        //boolean b = hasEnable(world,pos,state);
+        return gateConditionsMet(state, world, pos) ;//&& !b;
+    }
+
+
+}
---
+++ src/main/java/name/turingcomplete/multiblock/Half_Adder.java
@@ -0,0 +1,108 @@
+package name.turingcomplete.multiblock;
+
+import com.mojang.serialization.MapCodec;
+import name.turingcomplete.BLOCK_PART;
+import name.turingcomplete.MultiBlockGate;
+import net.minecraft.block.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
+import static net.minecraft.block.RedstoneWireBlock.POWER;
+
+
+public class Half_Adder extends MultiBlockGate {
+
+    public static final MapCodec<ComparatorBlock> CODEC = createCodec(ComparatorBlock::new);
+    public Half_Adder(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public MapCodec<ComparatorBlock> getCodec() {
+        return CODEC;
+    }
+
+    @Override
+    protected int getUpdateDelayInternal(BlockState state) {
+        return 2;
+    }
+
+    @Override
+    public boolean hasEnable(World world, BlockPos pos, BlockState state) {
+        if (state.get(PART) == BLOCK_PART.MIDDLE) {
+            Direction dir1 = state.get(FACING).rotateYCounterclockwise();
+            Direction dir2 = state.get(FACING).rotateYClockwise();
+            BlockPos pos1 = pos.offset(dir1);
+            BlockPos pos2 = pos.offset(dir2);
+            BlockState state1 = world.getBlockState(pos1);
+            BlockState state2 = world.getBlockState(pos2);
+            BlockPos SumPos = pos1.offset(dir1);
+            BlockState SumState = world.getBlockState(SumPos);
+            if (SumState.isOf(Blocks.REDSTONE_WIRE)) {
+                if (state1.get(HALFSUM) && state2.get(HALFSUM)) {
+                    world.setBlockState(SumPos, state.with(POWER, 15), Block.NOTIFY_LISTENERS);
+                    return true;
+                } else {
+                    world.setBlockState(SumPos, state.with(POWER, 0), Block.NOTIFY_LISTENERS);
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean gateConditionsMet(BlockState state, World world, BlockPos pos) {
+        /*if (state.get(PART) == BLOCK_PART.TOP){
+            if (getSideInput(world,state,pos) > 0) {
+                this.setHalfSum(world,pos,state,true);
+            } else {
+                this.setHalfSum(world,pos,state,false);
+            }
+        }
+        else if (state.get(PART) == BLOCK_PART.BOTTOM){
+            if (getSideInput(world,state,pos) > 0) {
+                this.setHalfSum(world,pos,state,true);
+            } else {
+                this.setHalfSum(world,pos,state,false);
+            }
+        }
+        if (state.get(PART) == BLOCK_PART.MIDDLE) {
+            world.setBlockState(pos, state.with(HALFSUM,false));
+            Direction dir1 = state.get(FACING).rotateYCounterclockwise();
+            Direction dir2 = state.get(FACING).rotateYClockwise();
+            BlockPos pos1 = pos.offset(dir1);
+            BlockPos pos2 = pos.offset(dir2);
+            BlockState state1 = world.getBlockState(pos1);
+            BlockState state2 = world.getBlockState(pos2);
+            if (state1.isOf(this) && state2.isOf(this)) {
+                return (state1.get(HALFSUM) ^ state2.get(HALFSUM));
+            }
+            return false;
+        }*/
+        return false;
+    }
+    
+    @Override
+    public Boolean dustConnectsToThis(BlockState state, Direction dir) {
+        //get gate state dir
+        Direction face_front = state.get(FACING);
+        if (state.get(PART) == BLOCK_PART.TOP || state.get(PART) == BLOCK_PART.BOTTOM) {
+            if (dir == face_front.getOpposite()){
+                return true;
+            }
+        }
+        if (state.get(PART) == BLOCK_PART.TOP && dir == face_front.rotateYClockwise()){
+            return true;
+        }
+        if (state.get(PART) == BLOCK_PART.MIDDLE && dir == face_front){
+            return true;
+        }
+        return false;
+    }
+
+    public void setHalfSum(World world, BlockPos pos, BlockState state, boolean value){
+        world.setBlockState(pos, state.with(HALFSUM,value));
+    }
+}
