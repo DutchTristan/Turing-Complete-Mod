10323b85e9ccc0aab06e91a5e5de6572dd225dad
DutchTristan <tristan@running-gag.nl>
2024-12-04 21:56:38 +0100

    Finalised data generation

--- src/main/java/name/turingcomplete/TuringCompleteDataGenerator.java
+++ src/main/java/name/turingcomplete/TuringCompleteDataGenerator.java
@@ -1,10 +1,13 @@
 package name.turingcomplete;
 
 import name.turingcomplete.data.provider.TuringCompleteRecipeProvider;
+import name.turingcomplete.data.provider.TuringCompleteLootTableProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.registry.RegistryBuilder;
 import net.minecraft.registry.RegistryKeys;
+
+import static net.minecraft.data.server.loottable.BlockLootTableGenerator.drops;
 
 public class TuringCompleteDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -12,6 +15,7 @@ public class TuringCompleteDataGenerator implements DataGeneratorEntrypoint {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
 		pack.addProvider(TuringCompleteRecipeProvider::new);
+		pack.addProvider(TuringCompleteLootTableProvider::new);
 	}
 
 	@Override
