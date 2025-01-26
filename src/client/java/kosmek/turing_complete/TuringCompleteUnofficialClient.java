fdfc0b09d0ec699b969b1e6bc311abc1f8b1252e
Kosmek Just Sometimes <154541438+KOSmekKosmik@users.noreply.github.com>
2025-01-26 11:18:34 +0100

    Add files via upload

---
+++ src/client/java/kosmek/turing_complete/TuringCompleteUnofficialClient.java
@@ -0,0 +1,10 @@
+package kosmek.turing_complete;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TuringCompleteUnofficialClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
