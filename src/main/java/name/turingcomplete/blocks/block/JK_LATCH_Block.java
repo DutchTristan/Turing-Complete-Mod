b6da35368f88ac3316140dd766c332890ab2a512
DutchTristan <tristan@running-gag.nl>
2025-02-04 12:35:16 +0100

    JK latch functionally implemented

---
+++ src/main/java/name/turingcomplete/blocks/block/JK_LATCH_Block.java
@@ -0,0 +1,58 @@
+package name.turingcomplete.blocks.block;
+
+import name.turingcomplete.blocks.AbstractSimpleLogicGate;
+import name.turingcomplete.init.propertyInit;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
+public class JK_LATCH_Block extends AbstractSimpleLogicGate {
+    private static final BooleanProperty SWAP = propertyInit.SWAPPED_DIR;
+
+    public JK_LATCH_Block(Settings settings) {
+        super(settings);
+        setDefaultState(getDefaultState()
+                .with(SWAP,false)
+        );
+    }
+
+    @Override
+    protected void properties(StateManager.Builder<Block, BlockState> builder)
+    {super.properties(builder);     builder.add(SWAP);}
+
+    /*
+         On Set (Left) : True
+         On Reset (Right) (Dominant) : False
+         On No Power In : SET Property Value
+     */
+
+    @Override
+    public boolean gateConditionMet( World world, BlockPos pos, BlockState state) {
+        boolean set = isInputPowered(world,state,pos,getSetDirection(state));
+        boolean reset = isInputPowered(world,state,pos,getSetDirection(state).getOpposite());
+
+        if (set && reset){
+            return !state.get(POWERED);
+        }
+        else if(!set && reset)
+            return false;
+        else if(set)
+            return true;
+
+        return state.get(POWERED);
+    }
+
+
+    @Override
+    public boolean supportsSideDirection(BlockState state, Direction direction) {return true;}
+    @Override
+    public boolean supportsBackDirection() {return false;}
+
+    private InputDirection getSetDirection(BlockState state){
+        return state.get(SWAP) ? InputDirection.LEFT : InputDirection.RIGHT;
+    }
+}
--- src/main/java/name/turingcomplete/init/blockInit.java
+++ src/main/java/name/turingcomplete/init/blockInit.java
@@ -100,6 +100,14 @@ public class blockInit {
                         .pistonBehavior(PistonBehavior.DESTROY)
                 )
             );
+
+    public static final JK_LATCH_Block JK_LATCH_BLOCK = registerWithItem("jk_latch_block",
+            new JK_LATCH_Block(AbstractBlock.Settings.create()
+                    .breakInstantly()
+                    .sounds(BlockSoundGroup.STONE)
+                    .pistonBehavior(PistonBehavior.DESTROY)
+            )
+    );
     //===============================================================================================
 
     public static final Half_Adder HALF_ADDER = registerWithItem("half_adder",
