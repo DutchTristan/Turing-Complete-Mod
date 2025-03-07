d187eb78ebbdad2de821d69d863ac7fd6edc1b68
Trevor Giddings <tgiddings000@gmail.com>
2025-03-06 21:11:45 -0800

    fix missing drop for memory cell

--- src/main/java/name/turingcomplete/data/provider/TuringCompleteLootTableProvider.java
+++ src/main/java/name/turingcomplete/data/provider/TuringCompleteLootTableProvider.java
@@ -46,6 +46,7 @@ public class TuringCompleteLootTableProvider extends FabricBlockLootTableProvide
         addDrop(blockInit.SR_LATCH_BLOCK, drops(blockInit.SR_LATCH_BLOCK));
         addDrop(blockInit.JK_LATCH_BLOCK, drops(blockInit.JK_LATCH_BLOCK));
         addDrop(blockInit.T_LATCH_BLOCK, drops(blockInit.T_LATCH_BLOCK));
+        addDrop(blockInit.MEMORY_CELL, drops(blockInit.MEMORY_CELL));
 
         addDrop(blockInit.HALF_ADDER, drop_if_property(blockInit.HALF_ADDER,propertyInit.BLOCK_PART,BLOCK_PART.MIDDLE));
         addDrop(blockInit.FULL_ADDER, drop_if_property(blockInit.FULL_ADDER,propertyInit.BLOCK_PART,BLOCK_PART.MIDDLE));
